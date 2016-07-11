1852b6ebfc14a3cc095c06db7700263fd4d6908f
Alex T Leach <alextleach@gmail.com>
2016-07-11 16:51:29 -0500

    Dan helped me fill out the left side

--- src/series-info-view.js
+++ src/series-info-view.js
@@ -3,6 +3,8 @@ export default class SeriesInfoView {
   constructor(element, newData) {
     this.series = newData.data.results[0];
     this.element = element;
+
+    this.renderCreators();
   }
 
 
@@ -12,4 +14,22 @@ export default class SeriesInfoView {
     this.element.querySelector('.left-container__year').innerText = `${this.series.startYear} - ${this.series.endYear}`;
     this.element.querySelector('.left-container__img').src = `${this.series.thumbnail.path}.${this.series.thumbnail.extension}`;
   }
+
+  renderCreators() {
+    this.series.creators.items.forEach(
+      (creator) => {
+        const creatorList = document.querySelector('.left-container__creators');
+        const creatorListItem = document.createElement('li');
+        creatorListItem.innerText = creator.name;
+        creatorList.appendChild(creatorListItem);
+      }
+    );
+    console.log(this.series.creators.items[0].name);
+  }
+
+    // for (var i = 0; i < this.series.creators.items.length; i++) {
+    //
+    // this.element.querySelector('.left-container__creators-item').innerText= this.series.creators.items[i].name;
+    //
+    // }
 }
