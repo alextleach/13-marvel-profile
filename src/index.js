6c32c93b6bdd896631835f44d1b9772cbb3434fd
Alex T Leach <alextleach@gmail.com>
2016-07-11 20:37:59 -0500

    got the characters to fetch now trying to get the img aspects right

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import SeriesInfoView from 'series-info-view';
+import CharacterListView from 'character-list-view';
 
 export default function start(pageElement) {
-  return fetch(`https://gateway.marvel.com/v1/public/series/9856?apikey=750be61f84711f443b3624f2d2e8175d`)
+  fetch(`https://gateway.marvel.com/v1/public/series/9856?apikey=750be61f84711f443b3624f2d2e8175d`)
     .then((res) => res.json())
     .then((data) => {
       const info = new SeriesInfoView(pageElement.querySelector('.left-container'), data);
       info.render();
+    });
+  fetch(`http://gateway.marvel.com:80/v1/public/series/9856/characters?apikey=750be61f84711f443b3624f2d2e8175d`)
+    .then((res) => res.json())
+    .then((data) => {
+      const y = new CharacterListView(pageElement.querySelector('.right-container__container-characters'), data);
+      y.render();
+      console.log(data);
     });
 }
